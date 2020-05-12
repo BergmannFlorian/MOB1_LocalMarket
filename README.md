ea589bee75a86ec19f21c5110adc7f74f36acedc
Florian Bergmann <flobergmann@aol.ch>
2020-05-12 09:39:37 +0200

    separate views in different component

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Module CPNV MOB1
 # Dependencies
 [React navigation stack](https://reactnavigation.org/docs/hello-react-navigation/)  
 [React navigation dependencies](https://reactnavigation.org/docs/getting-started)  
+
+# Run
+Android : 
+    
+    npx react-native run-android
+    npm run android
+
+# React native node module bug load
+
+    npm run android --reset-cache
---
+++ src/view/home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    StatusBar,
+    View,
+    Text
+  } from 'react-native';
+
+export const HomeView = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home View</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
