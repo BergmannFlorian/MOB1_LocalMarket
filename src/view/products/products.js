fbf876f0b94047d319a5561e65738d64c475aaa1
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-09-03 18:23:47 +0200

    fix response data products

--- src/view/products/products.js
+++ src/view/products/products.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import {StyleSheet,} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, View, Text, AsyncStorage } from 'react-native';
 
 export const ProductsView = ({ navigation }) => {
-  return (
-    <></>
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    const getProducts = (token) => {
+        fetch('http://10.229.33.29:81/api/products', {
+            method: 'GET',
+            headers: {"Authorization": "Bearer " + token}
+        })
+        .then((response) => response.json())
+        .then((json) => {
+            setData(json.data);
+            console.log(json.data);
+        })
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false))
+    };
+
+    _getToken = async () => {
+        const token = await AsyncStorage.getItem('@localmarket:token');
+        console.log(token);
+        if (token !== null) {
+            getProducts(token);
+        }
+    };
+    _getToken();
+
+
+    return (
+    <View>
+        {isLoading ? <ActivityIndicator/> : <Text>Chargé</Text>}
+    </View>
   );
 };
 
--- src/view/user/me.js
+++ src/view/user/me.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-native-elements'
 import {ActivityIndicator, StyleSheet, View, Text} from 'react-native';
 
 export const MeView = ({ route, navigation }) => {
@@ -22,6 +23,7 @@ export const MeView = ({ route, navigation }) => {
   return (
     <View style={{ flex: 1, padding: 24 }}>
       {isLoading ? <ActivityIndicator/> : <Text>{data.firstname} {data.lastname}</Text>}
+      <Button title="Products" onPress={() => navigation.navigate('Products')}/>
     </View>
   );
 };
