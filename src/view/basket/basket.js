8759a8a58c9cf82e0d4e71d5eae06914c97db3e3
Florian Bergmann <florian.bergmann@cpnv.ch>
2020-10-28 12:07:40 +0100

    fix add & remove from basket

--- src/view/basket/basket.js
+++ src/view/basket/basket.js
@@ -14,14 +14,14 @@ export const BasketView = ({ navigation }) => {
     const res = await axios.get(`${global.dbUrl}/api/products`, { headers: { Authorization: `Bearer ${token}` } });
     var temp_listProducts = res.data.data;
     var purchases = JSON.parse(await AsyncStorage.getItem('@localmarket:basket'));
-
+    console.log(purchases);
     if(res.status == 200){
-
       if(purchases != null){
         var temp_products = new Array;
         for (var index in purchases) {
           var product = temp_listProducts.find(product => product.id == purchases[index].product_id);
           if(product != undefined){
+            //move product from list to basket
             temp_products.push(product);
             temp_listProducts = temp_listProducts.filter(product => product.id != purchases[index].product_id);
           }
@@ -38,26 +38,28 @@ export const BasketView = ({ navigation }) => {
     if(purchases == null)purchases = new Array;
     if(purchases.find(product => product.product_id == id) == undefined){
       purchases.push({product_id: id, quantity: 0});
-      product_s = products;
-      product_s.push(listProducts.find(product => product.id == id));
-      setListProducts(listProducts.filter(product => product.id != id));
+      temp_products = products;
+      temp_products.push(listProducts.find(product => product.id == id));
       AsyncStorage.setItem('@localmarket:basket', JSON.stringify(purchases));
-      setProducts(product_s);
+      setListProducts(listProducts.filter(product => product.id != id));
+      setProducts(temp_products);
     }
   }
   async function removeFromBasket(id){
     var purchases = JSON.parse(await AsyncStorage.getItem('@localmarket:basket'));
-    listProduct_s = listProducts;
-    listProduct_s.push(products.find(product => product.id == id));
+    temp_listProducts = listProducts;
+    temp_listProducts.push(products.find(product => product.id == id));
+    AsyncStorage.setItem('@localmarket:basket', JSON.stringify(purchases.filter(product => product.product_id != id)));
     setProducts(products.filter(product => product.id != id));
-    AsyncStorage.setItem('@localmarket:basket', purchases.filter(product => product.product_id != id));
-    setListProducts(product_s);
+    setListProducts(temp_listProducts);
   }
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Basket View</Text>
       <Button title="Rafraichir" onPress={() => getProducts()}/>
+      <Button title="Ajouter" onPress={() => addToBasket(1)}/>
+      <Button title="Supprimer" onPress={() => removeFromBasket(1)}/>
     </View>
   );
 };
